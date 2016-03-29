3e4fc981d0fb784726f1e3292a28969326efa6f0
DenisRodionov <den.iu7@gmail.com>
2016-03-29 23:18:20 +0300

    project templated is added

---
+++ src/main/java/com/rodionov/cityoffice/CityofficeApplication.java
@@ -0,0 +1,12 @@
+package com.rodionov.cityoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityofficeApplication.class, args);
+	}
+}
