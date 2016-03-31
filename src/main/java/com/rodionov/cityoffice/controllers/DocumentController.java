3d15c04e709b81d45828969d2c218b5cef7b2da4
DenisRodionov <den.iu7@gmail.com>
2016-03-31 11:31:41 +0300

    angular navigation

--- src/main/java/com/rodionov/cityoffice/controllers/DocumentController.java
+++ src/main/java/com/rodionov/cityoffice/controllers/DocumentController.java
@@ -5,8 +5,6 @@ import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,7 +1,25 @@
-angular.module('app', [])
-  .controller('home', function($http) {
-    var self = this;
-    $http.get('/document/').then(function(response) {
-    	self.greeting = response.data;
-    });
-});
+angular.module('app', [ 'ngRoute' ])
+	  .config(function($routeProvider, $httpProvider) {
+	
+	    $routeProvider.when('/', {
+	      templateUrl : 'home.html',
+	      controller : 'home',
+	      controllerAs: 'controller'
+	    }).when('/login', {
+	      templateUrl : 'login.html',
+	      controller : 'navigation',
+	      controllerAs: 'controller'
+	    }).otherwise('/');
+	
+	    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+	
+	  })
+  	.controller('home', function($http) {
+  		var self = this;
+  		$http.get('/document/').then(function(response) {
+  			self.greeting = response.data;
+  		});
+    })
+  	.controller('navigation', function() { 
+  		
+  	});
