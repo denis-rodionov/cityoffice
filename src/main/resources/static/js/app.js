156146a3f54b91e12d038c98bd0fbe18677df46b
DenisRodionov <den.iu7@gmail.com>
2016-07-19 23:44:19 +0300

    angular translate added

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,4 +1,4 @@
-angular.module('app', [ 'ngRoute', 'services'])
+angular.module('app', [ 'ngRoute', 'services', 'pascalprecht.translate'])
 	  .config(function($routeProvider, $httpProvider) {
 		  
 		//$locationProvider.html5Mode(true);
@@ -25,68 +25,14 @@ angular.module('app', [ 'ngRoute', 'services'])
 	
 	    //$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
 	
-	  })    
-  	.controller('navigation',function($rootScope, $http, $location) {  		
-  		  console.log("navigation controller: " + $location.path());
-  		  
-  		  var self = this;
-  		  
-  		  $rootScope.menuClass = function(page) {
-  		    var current = $location.path().substring(1);
-  		    return page === current ? "active" : "";
-  		  };
-
-  		  var authenticate = function(credentials, callback) {
-  			  
-  			console.log("authenticate() { credentials : " + credentials + "}");
-
-  		    var headers = credentials ? {authorization : "Basic "
-  		        + btoa(credentials.username + ":" + credentials.password)
-  		    } : {};
-
-  		    console.log("sending request...");
-  		    $http.get('getuser', {headers : headers}).then(function(response) {
-  		      console.log("getuser succedd");
-  		      if (response.data.name) {
-  		        $rootScope.authenticated = true;
-  		      } else {
-  		        $rootScope.authenticated = false;
-  		      }
-  		      callback && callback();
-  		    }, function() {
-  		      console.log("getuser failed");
-  		    	
-  		      $rootScope.authenticated = false;
-  		      callback && callback();
-  		    });
-
-  		  }
-
-  		  authenticate();
-  		  self.credentials = {};
-  		  
-  		  self.login = function() {
-  			  console.log("login");
-  		      authenticate(self.credentials, function() {
-  		    	console.log("authentificate callback");
-  		    	
-  		    	
-  		        if ($rootScope.authenticated) {
-  		          console.log("redirection to documents");
-  		          $location.path("/");
-  		          self.error = false;
-  		        } else {
-  		          console.log("redirection to login page");
-  		          $location.path("/login");
-  		          self.error = true;
-  		        }
-  		      });
-  		  };
-  		  
-	  	  self.logout = function() {
-	  		  $http.post('/logout', {}).finally(function() {
-	  		    $rootScope.authenticated = false;
-	  		    $location.path("/");
-	  		  });
-	  		};
-  		});
+	  })  
+	 .config(['$translateProvider', function($translateProvider) {
+	    //$translateProvider.addInterpolation('$translateMessageFormatInterpolation');
+	
+	    $translateProvider.useStaticFilesLoader({
+	        prefix: '/i18n/',
+	        suffix: '.json?' + (new Date().getTime()) // no-cache for i18n
+	    });
+	    $translateProvider.preferredLanguage('ru');
+	}])
+  	
---
+++ src/main/resources/static/js/controllers/navigationController.js
@@ -0,0 +1,64 @@
+angular.module('app').controller('navigation',function($rootScope, $http, $location) {  		
+  		  console.log("navigation controller: " + $location.path());
+  		  
+  		  var self = this;
+  		  
+  		  $rootScope.menuClass = function(page) {
+  		    var current = $location.path().substring(1);
+  		    return page === current ? "active" : "";
+  		  };
+
+  		  var authenticate = function(credentials, callback) {
+  			  
+  			console.log("authenticate() { credentials : " + credentials + "}");
+
+  		    var headers = credentials ? {authorization : "Basic "
+  		        + btoa(credentials.username + ":" + credentials.password)
+  		    } : {};
+
+  		    console.log("sending request...");
+  		    $http.get('getuser', {headers : headers}).then(function(response) {
+  		      console.log("getuser succedd");
+  		      if (response.data.name) {
+  		        $rootScope.authenticated = true;
+  		      } else {
+  		        $rootScope.authenticated = false;
+  		      }
+  		      callback && callback();
+  		    }, function() {
+  		      console.log("getuser failed");
+  		    	
+  		      $rootScope.authenticated = false;
+  		      callback && callback();
+  		    });
+
+  		  }
+
+  		  authenticate();
+  		  self.credentials = {};
+  		  
+  		  self.login = function() {
+  			  console.log("login");
+  		      authenticate(self.credentials, function() {
+  		    	console.log("authentificate callback");
+  		    	
+  		    	
+  		        if ($rootScope.authenticated) {
+  		          console.log("redirection to documents");
+  		          $location.path("/");
+  		          self.error = false;
+  		        } else {
+  		          console.log("redirection to login page");
+  		          $location.path("/login");
+  		          self.error = true;
+  		        }
+  		      });
+  		  };
+  		  
+	  	  self.logout = function() {
+	  		  $http.post('/logout', {}).finally(function() {
+	  		    $rootScope.authenticated = false;
+	  		    $location.path("/");
+	  		  });
+	  		};
+  		});
