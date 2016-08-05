8cd646f2cee2dacb423c33d1de2be5d6cc2828de
DenisRodionov <den.iu7@gmail.com>
2016-08-05 09:44:51 +0300

    procfile

--- src/main/java/com/rodionov/cityoffice/services/DatabaseLoader.java
+++ src/main/java/com/rodionov/cityoffice/services/DatabaseLoader.java
@@ -2,12 +2,29 @@ package com.rodionov.cityoffice.services;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.rodionov.cityoffice.model.User;
+import com.rodionov.cityoffice.repository.UserRepository;
 
 @Service
 public class DatabaseLoader {
+	
+	@Autowired
+	private UserRepository userRepository;
 
     @PostConstruct
     private void initDatabase() {
+    	
+    	if (userRepository.findAll().size() == 0) {
+    		
+    		User defaultUser = new User();
+    		defaultUser.setEmail("admin@admin.com");
+    		defaultUser.setPassword("admin");
+    		defaultUser.setRole("ADMIN");
+    		
+    		userRepository.insert(defaultUser);
+    	}
     }
 }
