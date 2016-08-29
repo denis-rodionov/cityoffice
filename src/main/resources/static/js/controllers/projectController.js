4f22098dca108c9378e268bf725b3603abe13503
George Konyaev <georgii.konyaev@gmail.com>
2016-08-29 09:47:54 +0300

    some fixes

--- src/main/resources/static/js/controllers/projectController.js
+++ src/main/resources/static/js/controllers/projectController.js
@@ -1,24 +1,6 @@
 /**
  * Created by George on 19.08.2016.
  */
-/*angular.module('app').controller('projectController',
-	['EmployeeService',	function (EmployeeService) {
-
-		var self = this;
-		AmCharts.useUTC = true;
-
-		EmployeeService.getUserProjects()
-			.then(function(data) {
-					self.employees = data;
-					AmCharts.makeChart("chartdiv", data);
-
-				},
-
-				function(reason) {
-					self.error = reason;
-				});
-	}]);*/
-
 angular.module('app').controller('projectController',
 	['EmployeeService',	function (EmployeeService) {
 
@@ -32,23 +14,26 @@ angular.module('app').controller('projectController',
 				self.employees = data;
 				configdata = data;
 
+
 			},
 				function(reason) {
 					self.error = reason;
+				});
+
+		EmployeeService.getUserProjects()
+
+			.then(function (data) {
+
+				self.employees = data;
+
+				dst = {};
+				angular.extend(dst, data, configdata);
+				AmCharts.makeChart("chartdiv", dst);
+
+
 				})
-			.then(EmployeeService.getUserProjects()
-
-				.then(function (data) {
-
-					self.employees = data;
-					dst = {};
-					angular.extend(dst, data, configdata);
-					AmCharts.makeChart("chartdiv", dst);
 
 
-				})
-
-			);
 
 
 	}]);
