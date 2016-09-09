3f352f56e4ccabae804819e1792e6d85c7029bbe
DenisRodionov <den.iu7@gmail.com>
2016-09-09 13:23:41 +0300

    show only active projects on the project view #13

--- src/main/resources/static/js/services/projectService.js
+++ src/main/resources/static/js/services/projectService.js
@@ -4,14 +4,10 @@ angular.module('services')
         this.getDataProjects = function () {
             var deferred = $q.defer();
 
-            $http.get('/project')
+            $http.get('/project', { params: { isActive: true }})
                 .then(function (response) {
-
                     deferred.resolve(response.data);
-
-
                 }, function (error) {
-
                     deferred.reject('Error retrieving list of documents');
                 });
 
